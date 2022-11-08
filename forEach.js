8d5de5eb94d83f48e2df6e485c5f7c90851e653d
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-08 23:10:48 +0600

    discussed about forEach

---
+++ forEach.js
@@ -0,0 +1,12 @@
+const friends = ['tom cruise', 'tom braddy', 'tom solaiman', 'tom ayman', 'pussy najifa'];
+
+// friends.forEach(friends => console.log(friends));
+
+const products = [
+    { id: 1, name: 'laptop', price: 45000 },
+    { id: 2, name: 'phone', price: 80000 },
+    { id: 3, name: 'tablet', price: 35000 },
+    { id: 4, name: 'watcsh', price: 23000 }
+];
+
+products.forEach(products => console.log(products.price));
