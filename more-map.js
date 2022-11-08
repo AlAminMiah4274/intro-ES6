5e53919d13c33edc79f19d2f64d2a508b9ccc1a8
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-08 22:57:15 +0600

    discussed more about map

---
+++ more-map.js
@@ -0,0 +1,25 @@
+const numbers = [12, 46, 56, 87];
+const half = numbers.map(x => x / 2);
+const thirds = numbers.map(x => x / 3);
+
+// console.log(half);
+// console.log(thirds);
+
+const friends = ['safim rahman', 'rakib seikh', 'masum billah', 'sabbir howlader', 'rasel khan'];
+const firstLetters = friends.map(f => f[0]);
+// console.log(firstLetters);
+
+const nameLengths = friends.map(f => f.length);
+// console.log(nameLengths);
+
+const products = [
+    { id: 1, name: 'laptop', price: 45000 },
+    { id: 1, name: 'phone', price: 80000 },
+    { id: 1, name: 'tablet', price: 35000 },
+    { id: 1, name: 'watch', price: 45000 }
+];
+
+const items = products.map(p => p.name);
+// console.log(items);
+const productPrice = products.map(p => p.price);
+console.log(productPrice);
