9f274ff7c2d349d19609d588c1602b7f814bb5e7
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-11 22:37:16 +0600

    note down the module summary

---
+++ summary.js
@@ -0,0 +1,11 @@
+// 1. optional chaining 
+
+// 2. map(), filter() and find() (nust)
+
+// 3. forEach() and reduce (optional)
+
+// 4. class
+
+// 5. inheritance and super (optional)
+
+// 6. prototypical inheritance (must)
