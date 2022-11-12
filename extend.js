64d777a1bee43ecddd78a044da8335bb83cf8388
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-11-12 12:00:41 +0600

    practiced extends and super

--- extend.js
+++ extend.js
@@ -1,3 +1,70 @@
+// class TeamMember {
+//     name;
+//     location;
+//     constructor(name, location) {
+//         this.name = name;
+//         this.location = location;
+//     }
+//     provideFeedback() {
+//         console.log(`${this.name} thank you for your feedback.`);
+//     }
+// }
+// class Instructor extends TeamMember {
+//     designation = 'Web Course Instructor';
+//     team = 'Web Team';
+//     constructor(name, location) {
+//         super(name, location);
+//     }
+//     startSupportSession(time) {
+//         console.log(`Start the support session at ${time}`);
+//     }
+//     createQuiz(module) {
+//         console.log(`Please create quiz for module ${module}`);
+//     }
+// };
+
+
+// class Developer extends TeamMember {
+//     designation = 'Web Course Instructor';
+//     team = 'Web Team';
+//     tech;
+//     constructor(name, location, tech) {
+//         super(name, location);
+//         this.tech = tech;
+//     }
+//     developFeature(feature) {
+//         console.log(`Please develop the ${feature}`);
+//     }
+//     release(version) {
+//         console.log(`Please relase the version ${version}`);
+//     }
+// };
+
+
+// class JobPlacement extends TeamMember {
+//     designation = 'Job Placement Commandos';
+//     team = 'Job Placement';
+//     region;
+//     constructor(name, location, region) {
+//         super(name, location);
+//         this.region = region;
+//     }
+//     provideResume(time) {
+//         console.log(`Start the support session at ${time}`);
+//     }
+//     prepareStudent(module) {
+//         console.log(`Please create quiz for module ${module}`);
+//     }
+// };
+
+// const alia = new Developer('Alia Bhatt', 'Mumbai', 'React');
+// console.log(alia);
+// alia.provideFeedback();
+// const bipasha = new JobPlacement('Bipasha', 'Kolkata', 'India');
+// console.log(bipasha);
+
+// ********************* //
+
 class TeamMember {
     name;
     location;
@@ -8,11 +75,11 @@ class TeamMember {
     provideFeedback() {
         console.log(`${this.name} thank you for your feedback.`);
     }
-}
+};
+
 class Instructor extends TeamMember {
     designation = 'Web Course Instructor';
     team = 'Web Team';
-    tech;
     constructor(name, location) {
         super(name, location);
     }
@@ -24,23 +91,21 @@ class Instructor extends TeamMember {
     }
 };
 
-
 class Developer extends TeamMember {
-    designation = 'Web Course Instructor';
-    team = 'Web Team';
+    designation = 'FullStack Web Developer';
+    team = 'Team Warrior';
     tech;
     constructor(name, location, tech) {
         super(name, location);
         this.tech = tech;
     }
     developFeature(feature) {
-        console.log(`Please develop the ${feature}`);
+        console.log(`Please develop the feature ${feature}`);
     }
-    release(version) {
-        console.log(`Please relase the version ${version}`);
+    releaseVersion(version) {
+        console.log(`Please relaese the version ${version}`);
     }
 };
-
 
 class JobPlacement extends TeamMember {
     designation = 'Job Placement Commandos';
@@ -50,16 +115,14 @@ class JobPlacement extends TeamMember {
         super(name, location);
         this.region = region;
     }
-    provideResume(time) {
-        console.log(`Start the support session at ${time}`);
+    provideResume() {
+        console.log(`Please provide resume`);
     }
-    prepareStudent(module) {
-        console.log(`Please create quiz for module ${module}`);
+    prepareStudent() {
+        console.log(`Please prepare the students`);
     }
 };
 
-const alia = new Developer('Alia Bhatt', 'Mumbai', 'React');
-console.log(alia);
-alia.provideFeedback();
-const bipasha = new JobPlacement('Bipasha', 'Kolkata', 'India');
-console.log(bipasha);
+const alamin = new Developer('Al Amin Miah', 'Bangladesh', 'React');
+console.log(alamin);
+alamin.releaseVersion();
